00bc3311ead58b8b6c0ed4fcf7f30b08b3025d9b
krutikabhadane77 <krutikabhadane588@gmail.com>
2022-09-21 14:10:24 +0530

    Added in: Second Computation (a * b + c)

--- Sorting_Arithmatic_Computation.sh
+++ Sorting_Arithmatic_Computation.sh
@@ -2,4 +2,4 @@ read -p "Enter 1st number: " a
 read -p "Enter 2nd number: " b
 read -p "Enter 3rd number: " c
 echo "Three numbers are: $a $b $c"
-echo "$a + $b * $c = $(( a + b * c ))"
+echo "$a * $b + $c = $(( a * b + c ))"
